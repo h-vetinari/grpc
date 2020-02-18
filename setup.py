b01574aacab62b5674c4f467e29ec9ee4acfd1e8
Mike Sarahan <msarahan@gmail.com>
2020-02-18 13:53:05 -0600

    fix win setup cmds
    Co-Authored-By: Julien Schueller <schueller@phimeca.com>
    Co-Authored-By: Nicholas Bollweg <nick.bollweg@gmail.com>
    Co-Authored-By: Marius van Niekerk <marius.v.niekerk@gmail.com>
    Co-Authored-By: H. Vetinari <h.vetinari@gmx.com>

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ from distutils.unixccompiler import UnixCCompiler
 UnixCCompiler.src_extensions.append(".S")
 del UnixCCompiler
 
-from distutils import cygwinccompiler
 from distutils import extension as _extension
 from distutils import util
 import os
@@ -217,6 +216,9 @@ ENABLE_DOCUMENTATION_BUILD = _env_bool_value(
 
 
 def check_linker_need_libatomic():
+    if sys.platform == 'win32':
+        return False
+
     """Test if linker on system needs libatomic."""
     code_test = (
         b"#include <atomic>\n"
@@ -271,10 +273,6 @@ if EXTRA_ENV_COMPILE_ARGS is None:
                 EXTRA_ENV_COMPILE_ARGS += (
                     " -D_ftime=_ftime64 -D_timeb=__timeb64"
                 )
-        else:
-            # We need to statically link the C++ Runtime, only the C runtime is
-            # available dynamically
-            EXTRA_ENV_COMPILE_ARGS += " /MT"
     elif "linux" in sys.platform:
         EXTRA_ENV_COMPILE_ARGS += (
             " -fvisibility=hidden -fno-wrapv -fno-exceptions"
@@ -292,6 +290,7 @@ if EXTRA_ENV_LINK_ARGS is None:
         if check_linker_need_libatomic():
             EXTRA_ENV_LINK_ARGS += " -latomic"
     elif "win32" in sys.platform and sys.version_info < (3, 5):
+        from distutils import cygwinccompiler
         msvcr = cygwinccompiler.get_msvcr()[0]
         EXTRA_ENV_LINK_ARGS += (
             " -static-libgcc -static-libstdc++ -mcrtdll={msvcr}"
